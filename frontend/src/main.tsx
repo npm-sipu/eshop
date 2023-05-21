4698209c24bdb053b50671cb53f620c2a215c7e0
npm-sipu <sujit.bhatta95@gmail.com>
2023-05-21 14:14:38 +0530

    chnaged some code

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,6 @@ import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
 import store from "./store/store";
-import "./axiosConfig";
 
 const queryClient = new QueryClient();
 
